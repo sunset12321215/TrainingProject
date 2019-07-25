fdba0be710b7042ba2f1818a0380fdb3392658b2
Sun <sunsetno08@gmail.com>
2019-07-25 15:08:53 +0700

    Init Project

---
+++ TrainingProject/Support/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  TrainingProject
+//
+//  Created by CuongVX-D1 on 7/25/19.
+//  Copyright © 2019 CuongVX-D1. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
